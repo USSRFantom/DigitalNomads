4f1c87ed70cce21aab53eea7bfdf3c6445e61abb
Дмитрий <dimkakrohin@mail.ru>
2019-11-11 12:02:38 +0300

    First commit + ready SplashScreen

---
+++ app/src/main/java/com/hfad/digitalnomads/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.hfad.digitalnomads;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+    private final int SPLASH_SCREEN = 5000; //5 секунд
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        },SPLASH_SCREEN);
+    }
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+    }
+}
